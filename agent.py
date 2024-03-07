be95de9e6b2dc6308ca0b94ddd19ad39e8ea9ff8
shibaTT <39818834+shibaTT@users.noreply.github.com>
2024-03-07 12:55:16 +0900

    feat: Geminiと接続してAIに返答を生成する機能の追加

---
+++ agent.py
@@ -0,0 +1,74 @@
+import pickle
+import os
+from gemini import Gemini
+from atproto import Client, models
+
+
+class BskyAgent():
+    def __init__(self, client: Client, gemini: Gemini):
+        self.client = client
+        self.gemini = gemini
+
+        # is_file = os.path.isfile("posts.pickle")
+        # if not is_file:
+        #     f = open("posts.pickle", "wb")
+        #     f.close()
+
+    def read_post_file(self) -> list[str]:
+        if not os.path.isfile("posts.pickle"):
+            return []
+
+        # print(pickle.load(open("posts.pickle", "rb")))
+        return pickle.load(open("posts.pickle", "rb"))
+
+    def read_responded_file(self) -> set[str]:
+        if not os.path.isfile("responded.pickle"):
+            return set()
+
+        # print(pickle.load(open("responded.pickle", "rb")))
+        return pickle.load(open("responded.pickle", "rb"))
+
+    def write_post_file(self, uri: str):
+        posts = self.read_file()
+        with open("posts.pickle", "wb") as f:
+            posts.append(uri)
+            pickle.dump(posts, f)
+
+    def write_responded_file(self, uri: str):
+        responded = self.read_responded_file()
+        with open("responded.pickle", "wb") as f:
+            responded.add(uri)
+            pickle.dump(responded, f)
+
+    def login_bonus(self):
+        post = self.client.send_post(text="tesuto dayoooooOO")
+        self.write_post_file(post.uri)
+
+    def read_replies(self):
+        posts = self.read_post_file()
+
+        for post in posts:
+            res = self.client.get_post_thread(post, 1)
+            # res_dict = models.get_model_as_dict(res)
+            # print(res_dict["thread"]["replies"])
+            # for reply in res_dict["thread"]["replies"]:
+            # print(reply["post"]["cid"], reply["post"]["uri"])
+            # print(models.get_or_create(reply["post"]))
+            for reply in res.thread.replies:
+                # print(reply)
+                if reply.post.uri in self.read_responded_file():
+                    continue
+
+                try:
+                    reply_text = self.gemini.generate_response()
+                    message = f"ログインボーナス！今日は「{reply_text}」をプレゼントするわ！"
+
+                    reply_model_ref = models.create_strong_ref(reply.post)
+                    reply_to = models.AppBskyFeedPost.ReplyRef(parent=reply_model_ref, root=reply_model_ref)
+                    self.client.send_post(message, reply_to=reply_to)
+                    self.write_responded_file(reply.post.uri)
+
+                    print("Successfully Posted.")
+                except:
+                    print("Failed to Post.")
+                    continue
--- main.py
+++ main.py
@@ -1,82 +1,33 @@
 import os
-import pickle
+import schedule
+from time import sleep
 from dotenv import load_dotenv
-from atproto import Client, models
-
-load_dotenv("./.env.local")
-
-# print(os.getenv('BS_USERNAME'))
+from atproto import Client
+from agent import BskyAgent
+from gemini import Gemini
 
 
-class BskyAgent:
-    def __init__(self, client: Client):
-        self.client = client
-
-        # is_file = os.path.isfile("posts.pickle")
-        # if not is_file:
-        #     f = open("posts.pickle", "wb")
-        #     f.close()
-
-    def read_post_file(self) -> list[str]:
-        if not os.path.isfile("posts.pickle"):
-            return []
-
-        print(pickle.load(open("posts.pickle", "rb")))
-        return pickle.load(open("posts.pickle", "rb"))
-
-    def read_responded_file(self) -> set[str]:
-        if not os.path.isfile("responded.pickle"):
-            return set()
-
-        print(pickle.load(open("responded.pickle", "rb")))
-        return pickle.load(open("responded.pickle", "rb"))
-
-    def write_post_file(self, uri: str):
-        posts = self.read_file()
-        with open("posts.pickle", "wb") as f:
-            posts.append(uri)
-            pickle.dump(posts, f)
-
-    def write_responded_file(self, uri: str):
-        responded = self.read_responded_file()
-        with open("responded.pickle", "wb") as f:
-            responded.add(uri)
-            pickle.dump(responded, f)
-
-    def login_bonus(self):
-        post = self.client.send_post(text="tesuto dayoooooOO")
-        self.write_post_file(post.uri)
-
-    def read_replies(self):
-        posts = self.read_post_file()
-
-        for post in posts:
-            res = self.client.get_post_thread(post, 1)
-            # res_dict = models.get_model_as_dict(res)
-            # print(res_dict["thread"]["replies"])
-            # for reply in res_dict["thread"]["replies"]:
-            # print(reply["post"]["cid"], reply["post"]["uri"])
-            # print(models.get_or_create(reply["post"]))
-            for reply in res.thread.replies:
-                if reply.post.uri in self.read_responded_file():
-                    continue
-
-                reply_model_ref = models.create_strong_ref(reply.post)
-                reply_to = models.AppBskyFeedPost.ReplyRef(parent=reply_model_ref, root=reply_model_ref)
-                self.client.send_post("リプライのテスト", reply_to=reply_to)
-                self.write_responded_file(reply.post.uri)
+load_dotenv(".env.local")
 
 
 def main():
     client = Client()
     client.login(os.getenv('BS_USERNAME'), os.getenv('BS_PASSWORD'))
 
-    agent = BskyAgent(client)
+    gemini = Gemini()
+    agent = BskyAgent(client, gemini)
     # agent.read_post_file()
-    agent.read_replies()
 
-    # 1日1回ログインボーナス
-    # agent.login_bonus()
+    # 1分おきにリプライ確認
+    schedule.every(60).seconds.do(agent.read_replies)
+
+    # 1日1回（18時）ログインボーナス
+    schedule.every().days.at("18:00").do(agent.login_bonus)
+
+    while True:
+        schedule.run_pending()
+        # print(gemini.generate_response())
+        sleep(5)
 
 
 if __name__ == "__main__":
@@ -87,10 +38,10 @@ if __name__ == "__main__":
 #     ThreadViewPost(
 #         post=PostView(
 #             author=ProfileViewBasic(
-#                 did="did:plc:iyexec3xekkwdqheumvv2u7q",
-#                 handle="torichan.bsky.social",
-#                 avatar="https://cdn.bsky.app/img/avatar/plain/did:plc:iyexec3xekkwdqheumvv2u7q/bafkreicgm2bxiwifw7qtc4j3o6d7bpjmec6m2ulchkitlycuypmojb42qy@jpeg",
-#                 display_name="Bird in the bluesky",
+#                 did="did:plc:xxxxx",
+#                 handle="xxxxx.bsky.social",
+#                 avatar="https://xxxxx",
+#                 display_name="xxxxx",
 #                 labels=[],
 #                 viewer=ViewerState(
 #                     blocked_by=False,
@@ -104,11 +55,11 @@ if __name__ == "__main__":
 #                 ),
 #                 py_type="app.bsky.actor.defs#profileViewBasic",
 #             ),
-#             cid="bafyreico3sgl7meg3o4hymsmq72bkcziwcc7qwkulppoci7uxxvplgp64q",
+#             cid="xxxxx",
 #             indexed_at="2024-03-06T04:28:58.990Z",
 #             record=Record(
 #                 created_at="2024-03-06T04:28:58.990264+00:00",
-#                 text="tesuto dayoooooOO",
+#                 text="xxxxx",
 #                 embed=None,
 #                 entities=None,
 #                 facets=None,
@@ -118,7 +69,7 @@ if __name__ == "__main__":
 #                 tags=None,
 #                 py_type="app.bsky.feed.post",
 #             ),
-#             uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmytmwl7fs2r",
+#             uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #             embed=None,
 #             labels=[],
 #             like_count=1,
@@ -126,7 +77,7 @@ if __name__ == "__main__":
 #             repost_count=0,
 #             threadgate=None,
 #             viewer=ViewerState(
-#                 like="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.like/3kmyto55lfs2f",
+#                 like="at://did:plc:xxxxx/app.bsky.feed.like/xxxxx",
 #                 reply_disabled=None,
 #                 repost=None,
 #                 py_type="app.bsky.feed.defs#viewerState",
@@ -138,9 +89,9 @@ if __name__ == "__main__":
 #             ThreadViewPost(
 #                 post=PostView(
 #                     author=ProfileViewBasic(
-#                         did="did:plc:iyexec3xekkwdqheumvv2u7q",
+#                         did="did:plc:xxxxx",
 #                         handle="torichan.bsky.social",
-#                         avatar="https://cdn.bsky.app/img/avatar/plain/did:plc:iyexec3xekkwdqheumvv2u7q/bafkreicgm2bxiwifw7qtc4j3o6d7bpjmec6m2ulchkitlycuypmojb42qy@jpeg",
+#                         avatar="https://xxxxx",
 #                         display_name="Bird in the bluesky",
 #                         labels=[],
 #                         viewer=ViewerState(
@@ -155,7 +106,7 @@ if __name__ == "__main__":
 #                         ),
 #                         py_type="app.bsky.actor.defs#profileViewBasic",
 #                     ),
-#                     cid="bafyreidmb3ntj3umqalu2huhyd7vslsm6q74wvz5ny5k66sb7quz55cjhq",
+#                     cid="xxxxx",
 #                     indexed_at="2024-03-06T04:37:30.353Z",
 #                     record=Record(
 #                         created_at="2024-03-06T04:37:30.353Z",
@@ -167,13 +118,13 @@ if __name__ == "__main__":
 #                         langs=["ja"],
 #                         reply=ReplyRef(
 #                             parent=Main(
-#                                 cid="bafyreico3sgl7meg3o4hymsmq72bkcziwcc7qwkulppoci7uxxvplgp64q",
-#                                 uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmytmwl7fs2r",
+#                                 cid="xxxxx",
+#                                 uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                                 py_type="com.atproto.repo.strongRef",
 #                             ),
 #                             root=Main(
-#                                 cid="bafyreico3sgl7meg3o4hymsmq72bkcziwcc7qwkulppoci7uxxvplgp64q",
-#                                 uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmytmwl7fs2r",
+#                                 cid="xxxxx",
+#                                 uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                                 py_type="com.atproto.repo.strongRef",
 #                             ),
 #                             py_type="app.bsky.feed.post#replyRef",
@@ -181,7 +132,7 @@ if __name__ == "__main__":
 #                         tags=None,
 #                         py_type="app.bsky.feed.post",
 #                     ),
-#                     uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmyu47m7xk2g",
+#                     uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                     embed=None,
 #                     labels=[],
 #                     like_count=0,
@@ -203,9 +154,9 @@ if __name__ == "__main__":
 #             ThreadViewPost(
 #                 post=PostView(
 #                     author=ProfileViewBasic(
-#                         did="did:plc:iyexec3xekkwdqheumvv2u7q",
+#                         did="did:plc:xxxxx",
 #                         handle="torichan.bsky.social",
-#                         avatar="https://cdn.bsky.app/img/avatar/plain/did:plc:iyexec3xekkwdqheumvv2u7q/bafkreicgm2bxiwifw7qtc4j3o6d7bpjmec6m2ulchkitlycuypmojb42qy@jpeg",
+#                         avatar="https://cdn.bsky.app/img/avatar/plain/did:plc:xxxxx/xxxxx@jpeg",
 #                         display_name="Bird in the bluesky",
 #                         labels=[],
 #                         viewer=ViewerState(
@@ -220,11 +171,11 @@ if __name__ == "__main__":
 #                         ),
 #                         py_type="app.bsky.actor.defs#profileViewBasic",
 #                     ),
-#                     cid="bafyreigd522y2roocfkszije5spi2ha4b2feeakv35jnr6n2vv5jubh7v4",
+#                     cid="xxxxx",
 #                     indexed_at="2024-03-06T04:29:35.627Z",
 #                     record=Record(
 #                         created_at="2024-03-06T04:29:35.627Z",
-#                         text="sou nanoooooOO",
+#                         text="xxxxx",
 #                         embed=None,
 #                         entities=None,
 #                         facets=None,
@@ -232,13 +183,13 @@ if __name__ == "__main__":
 #                         langs=["ja"],
 #                         reply=ReplyRef(
 #                             parent=Main(
-#                                 cid="bafyreico3sgl7meg3o4hymsmq72bkcziwcc7qwkulppoci7uxxvplgp64q",
-#                                 uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmytmwl7fs2r",
+#                                 cid="xxxxx",
+#                                 uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                                 py_type="com.atproto.repo.strongRef",
 #                             ),
 #                             root=Main(
-#                                 cid="bafyreico3sgl7meg3o4hymsmq72bkcziwcc7qwkulppoci7uxxvplgp64q",
-#                                 uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmytmwl7fs2r",
+#                                 cid="xxxxx",
+#                                 uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                                 py_type="com.atproto.repo.strongRef",
 #                             ),
 #                             py_type="app.bsky.feed.post#replyRef",
@@ -246,7 +197,7 @@ if __name__ == "__main__":
 #                         tags=None,
 #                         py_type="app.bsky.feed.post",
 #                     ),
-#                     uri="at://did:plc:iyexec3xekkwdqheumvv2u7q/app.bsky.feed.post/3kmyto2vjqk2b",
+#                     uri="at://did:plc:xxxxx/app.bsky.feed.post/xxxxx",
 #                     embed=None,
 #                     labels=[],
 #                     like_count=0,
